355298f3d8a24e4963626febcb3152156179e982
Kristofer Hallin <kristofer@sunet.se>
2020-02-04 20:24:21 +0100

    Documentation and random fixes.

--- model.py
+++ model.py
@@ -5,34 +5,43 @@ from cli_struct import f_cli
 class Cli():
     def __init__(self, file):
         """ Contructor """
+
         self.__yaml_read(file)
 
     def __yaml_read(self, file):
         """ Parse the YAML file and create a Cli class """
+
         with open(file, 'r') as fd:
             self.json = yaml.safe_load(fd)
         self.cli = f_cli(**self.json)
 
-    def commands(self):
+    def get_commands(self):
         """ Return a list of command names """
+
         return [x.command.name for x in self.cli.cli]
 
-    def command_description(self, command):
+    def get_command_description(self, command):
         """ Return descirption of a specific command """
-        return [x.command.description for x in self.cli.cli if x.command.name == command][0]
+
+        return [x.command.description for x in self.cli.cli if x.command.name
+                == command][0]
 
     def get_attributes(self, command):
         """ Return attribute names for a command """
+
         cmd_attrs = []
         for cmd in self.cli.cli:
             if cmd.command.name != command:
                 continue
+            if cmd.command.attributes is None:
+                return None
             for attr in cmd.command.attributes:
                 cmd_attrs.append(attr.name)
         return cmd_attrs
 
     def get_mandatory(self, command):
         """ Return whether an attribute is mandatory or not """
+
         mandatory = []
         for cmd in self.cli.cli:
             if cmd.command.name != command:
@@ -44,6 +53,7 @@ class Cli():
 
     def get_description(self, command, attribute):
         """ Return descirption for an attribute """
+
         description = ''
         for cmd in self.cli.cli:
             if cmd.command.name != command:
@@ -56,6 +66,7 @@ class Cli():
 
     def get_url(self, command):
         """ Return descirption for an attribute """
+
         url = ''
         for cmd in self.cli.cli:
             if cmd.command.name != command:
@@ -65,6 +76,7 @@ class Cli():
 
     def get_methods(self, command):
         """ Return attribute names for a command """
+
         cmd_methods = []
         for cmd in self.cli.cli:
             if cmd.command.name != command:
@@ -74,9 +86,13 @@ class Cli():
         return cmd_methods
 
     def get_base_url(self):
+        """ Return base URL """
+
         return self.cli.base_url
 
     def get_url_suffix(self, command, attribute):
+        """ Find out if an argument should be a suffix to the URL or not. """
+
         for cmd in self.cli.cli:
             if cmd.command.name != command:
                 continue
@@ -85,19 +101,3 @@ class Cli():
                     continue
                 return attr.url_suffix
         return None
-
-
-if __name__ == '__main__':
-    c = Cli('cnaas.yml')
-    print(c.get_base_url())
-    for cmd in c.commands():
-        print(c.command_description(cmd))
-        print(c.get_mandatory(cmd))
-        print(c.get_methods(cmd))
-        print(c.get_url(cmd))
-
-        if c.get_attributes(cmd) is None:
-            continue
-        for attr in c.get_attributes(cmd):
-            print(c.get_description(cmd, attr))
-            print(c.get_url_suffix(cmd, attr))
